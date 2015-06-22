7cd5a8278b35202d19345a6f5a439b0e9c737ccb
shaojie.xu <shaojie.xu@infinitagroup.it>
2015-06-22 16:19:33 +0200

    add spring security

--- src/main/java/com/infinitagroup/www/controller/HomeController.java
+++ src/main/java/com/infinitagroup/www/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.infinitagroup.www.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -17,6 +20,29 @@ public class HomeController {
     	    	    	
         return "index";
     }
+    
+    
+    @RequestMapping(value="/main", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model, Principal principal ) {
+    String name = principal.getName();
+    model.addAttribute("username", name);
+    return "main_page";
+    }
+   
+    @RequestMapping(value="/login", method = RequestMethod.GET)
+    public String login(ModelMap model) {
+   
+    return "login_page";
+   
+    }
+   
+    @RequestMapping(value="/loginError", method = RequestMethod.GET)
+    public String loginError(ModelMap model) {
+    model.addAttribute("error", "true");
+    return "login_page";
+  
+    }
+
 
 	
 }
---
+++ src/main/webapp/WEB-INF/views/main_page.jsp
@@ -0,0 +1,9 @@
+<%@ page isELIgnored="false" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<body>
+Welcome <c:out value="">${username}</c:out>!
+<br/>
+This is the main page!
+</body>
+</html>
